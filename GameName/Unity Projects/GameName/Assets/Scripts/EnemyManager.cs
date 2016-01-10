4a4c4a3a8b7b1565eb47c2e4eb652180b7f958f2
anorman149 <anormanmail@gmail.com>
2016-01-09 21:58:19 -0500

    Spawning
    Fixed how the enemies were allowed to spawn under the player

--- GameName/Unity Projects/GameName/Assets/Scripts/EnemyManager.cs
+++ GameName/Unity Projects/GameName/Assets/Scripts/EnemyManager.cs
@@ -16,13 +16,16 @@ public class EnemyManager : MonoBehaviour {
     }
 
     void Spawn() {
-        // Don't spawn if dead
-        if(player.IsDead()) {
-            return;
-        }
-
         // Find a random index between zero and one less than the number of spawn points.
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        //Check to see if an Object is in the way
+        bool objectThere = Physics2D.OverlapCircle(spawnPoints[spawnPointIndex].position, .5f);
+
+        // Don't spawn if dead OR an Object is there
+        if(player.IsDead() || objectThere) {
+            return;
+        }
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
